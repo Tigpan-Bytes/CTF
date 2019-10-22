8dd683374dd4fbbb464fac78e6abcf134b0f9941
Timothy Letkeman <tiggy02@gmail.com>
2019-10-22 14:11:15 -0600

    php

--- index.php
+++ index.php
@@ -20,4 +20,10 @@
       <span style='float:left'>Capture the Flag: <span style='color:grey'>|</span> Jeopardy > Crypto</span> 
       <span style='float:right'>UserName <span style='color:grey'>|</span> Logout</span> 
     </span>
+
+    <form action="testing.php" method="post">
+      Name: <input type="text" name="name"><br>
+      E-mail: <input type="text" name="email"><br>
+      <input type="submit">
+    </form>
 </body>

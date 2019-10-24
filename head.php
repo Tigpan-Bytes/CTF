e87623b763a7507e6547222b3b5021a4d462ae83
BuildTools <unconfigured@null.spigotmc.org>
2019-10-23 22:06:49 -0600

    design stuff

--- head.php
+++ head.php
@@ -3,6 +3,7 @@
 
     <link id="mainstyle" rel="stylesheet" type="text/css" href="css/style.css">
     <link id="titlestyle" rel="stylesheet" type="text/css" href="css/title.css">
+    <link href="https://fonts.googleapis.com/css?family=Anonymous+Pro&display=swap" rel="stylesheet"> 
 
 	<title> CTF </title>
 </head>
--- main.php
+++ main.php
@@ -20,7 +20,12 @@
 	<?php include 'page-title.php';?>
     <p>This is the main page for only people who have logged in. In the future this will link to the jeopardy and the ai comp. 
     Also on the right side bar (if you are not in a challenge), the scores for each team. Base the design off <a href="https://github.com/UnrealAkama/NightShade">this</a>.</p>
+    <h2>Jeopardy</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt natus optio quas culpa tenetur. 
+    Quibusdam eos quaerat distinctio iure error architecto adipisci dolores veniam, nihil voluptas, dolor ex reprehenderit aliquid?</p>
 
+    <p2>
     Welcome <?php echo getSessionVar('username'); ?><br>
     Your password is: <?php echo getSessionVar('password'); ?>
+    </p2>
 </body>

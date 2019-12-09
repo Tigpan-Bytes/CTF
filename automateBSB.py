bb1a5455d98593f75f42182857089e449d6fa416
Timothy Letkeman <tiggy02@gmail.com>
2019-12-09 14:12:42 -0600

    worked on automation

--- automateBSB.py
+++ automateBSB.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import importlib
 from tkinter import filedialog
 from os import listdir, unlink
 from os.path import isfile, join
@@ -7,17 +8,22 @@ from shutil import copy
 root = tk.Tk()
 root.withdraw()
 
-bot_path = filedialog.askdirectory() + '/bots'
+main_path = filedialog.askdirectory()
+bot_path = './bots'
+
 for file in listdir(bot_path):
     file_loc = join(bot_path, file)
     if isfile(file_loc):
         unlink(file_loc)
         
-for dir in listdir('.'):
-    dir_loc = join('.', dir)
+for dir in listdir(main_path):
+    dir_loc = join(main_path, dir)
     if not isfile(dir_loc):
-        for file in listdir(dir):
+        for file in listdir(dir_loc):
             file_loc = join(dir_loc, file)
             if isfile(file_loc):
                 copy(file_loc, bot_path)
+                
+game = importlib.import_module('main')
         
+
